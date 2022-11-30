2ef70bc2d8e36787c5076fff12f9417a95120fed
snowari <pes1149@naver.com>
2022-12-01 00:38:09 +0900

    새로고침해도 로그아웃되지 않게 수정

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+const { localStorage } = window;
+
 const useAuth = () => {
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const wasAuthenticated = localStorage.getItem("isAuthenticated");
+
+  if (wasAuthenticated) {
+    setIsAuthenticated(true);
+  }
 
   const handleLogin = () => {
     setIsAuthenticated(true);
+    localStorage.setItem("isAuthenticated", "true");
   };
 
   const handleLogout = () => {
     setIsAuthenticated(false);
+    localStorage.removeItem("isAuthenticated");
   };
 
   useEffect(() => {
